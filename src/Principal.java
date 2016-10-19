8fa41824885e39f73d6e77caad71b1fad04ca03c
UserPL135Pc05 <UserPL135Pc05@SLFRPL135PC05.ensino.atec.local>
2016-10-19 12:04:36 +0100

    Aula 0.

--- src/Principal.java
+++ src/Principal.java
@@ -13,15 +13,17 @@ public class Principal {
 		
 		System.out.println(mensagem);
 		
-		int agua = 40; 
-		int luz = 20; 
-		int gas = 20;
-		int fibra = 60;
+		int agua 	= 40; 
+		int luz 	= 20; 
+		int gas 	= 20;
+		int fibra 	= 60;
 		
-		//agua	 = 40;
-		//luz	 	 = 20;
-		//gas 	 = 20;
-		//fibra 	 = 60; 
+		/*
+		agua	 = 40;
+		luz	 	 = 20;
+		gas 	 = 20;
+		fibra 	 = 60; 
+		*/
 		
 		int despesas = agua + luz + gas + fibra;
 		// TODO MOSTRAR NO ECRA O TOTAL DE DESPESAS
